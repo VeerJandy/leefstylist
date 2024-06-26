e09e19866907269e700575d9d5b24472d47abab8
VeerJandy <tagen5123@gmail.com>
2024-06-26 18:05:38 +0300

    Header

--- src/modules/header/components/Header.tsx
+++ src/modules/header/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => (
   <header className="bg-white/80 fixed z-header w-full backdrop-blur-lg backdrop-saturate-[180%]">
     <nav className="container flex h-16 items-center justify-between border-b border-light">
       <Link href="/" className="h-10">
-        <Image src={logoImg} alt="logo" />
+        <Image src={logoImg} alt="logo" priority quality={100} />
       </Link>
       <Button color="white">Account</Button>
     </nav>
