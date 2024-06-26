208df26143bebe8df8738361ff3c137e6035113e
VeerJandy <tagen5123@gmail.com>
2024-06-26 18:07:14 +0300

    Remove i18n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import '~/styles/index.css'
 
-// import { promises as fs } from 'fs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
 
 import { Header } from '~/modules/header'
-
-// import type { Locale } from '~/i18n/i18n'
 
 const font = Inter({
   weight: ['400', '600'],
@@ -22,23 +19,15 @@ export const metadata: Metadata = {
 
 interface RootLayoutProps {
   children: ReactNode
-  // params: { lang: Locale }
 }
 
-const RootLayout = async ({ children }: RootLayoutProps) => {
-  // const dictionary = await fs.readFile(
-  //   process.cwd() + `/src/i18n/dictionaries/${params.lang}.json`,
-  //   'utf8'
-  // )
-
-  return (
-    <html lang="en" className={font.className}>
-      <body className="scroll-bar">
-        <Header />
-        <main>{children}</main>
-      </body>
-    </html>
-  )
-}
+const RootLayout = async ({ children }: RootLayoutProps) => (
+  <html lang="en" className={font.className}>
+    <body className="scroll-bar">
+      <Header />
+      <main>{children}</main>
+    </body>
+  </html>
+)
 
 export default RootLayout
